53760bf1975a5e7d3650e8e954f0abc3ca34889e
HeTvaM <guddr@mail.ru>
2022-07-12 23:58:38 +0300

    update connection.py

--- services/app/db_connection/connection.py
+++ services/app/db_connection/connection.py
@@ -1,39 +1,44 @@
 from pymongo import MongoClient
+from pymongo.errors import OperationFailure, PyMongoError
+
+def check_failure(func):
+    def wrapper(*args, **kwargs):
+        try:
+            res = func(args, kwargs)
+        except (AutoReconnect, ConnectionFailure) as e:
+            return e, False
+
+        return res, True
 
 
 class Connection:
+    OPEN_TABLE_NAME="open"
+    CLOSE_TABLE_NAME="close"
 
-    def __init__(self, connection_data, db_name, open_table, super_table):
-        self.open_table = Table(connection_data, db_name, open_table)
-        self.__super_table = Table(connection_data, db_name, super_table)
-        pass
+    def __init__(self, connection_data, db_name):
+        self._open_table = Table(connection_data, db_name, self.OPEN_TABLE_NAME)
+        self._close_table = Table(connection_data, db_name, self.CLOSE_TABLE_NAME)
 
-    def add_super_block(self, super_block):
-        # delete block from open table self.open_table.delete_block()
-        self.__super_table.add_block(super_block)
-        pass
-
-    pass
+    def close_table_add(self, super_block):
+        self._close_table.add_block(super_block)
 
 
 class Table:
-
     def __init__(self, connection_data, db_name, table_name):
-        self.__table = MongoClient(connection_data)[db_name][table_name]
-        pass
+        self._table = MongoClient(connection_data)[db_name][table_name]
 
+    @check_failure
     def add_block(self, block):
-        self.__table.insert_one(block)
-        pass
+        self._table.insert_one(block)
 
-    def delete_block(self, block):
-        self.__table.delete_one(block)
-        pass
+    @check_failure
+    def del_block(self, block):
+        self._table.delete_one(block)
 
-    def show_table(self):
-        return self.__table.find()
+    @check_failure
+    def get_table(self):
+        return self._table.find()
 
+    @check_failure
     def get_last_block(self):
-        return self.__table.find().sort('_id', -1)[0]
-
-    pass
+        return self._table.find().sort('_id', -1)[0]
